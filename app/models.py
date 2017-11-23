d85182673eafec06a87fa13e51701a872af9495a
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2017-11-23 14:59:36 -0600

    Updated models with title and copies

--- app/models.py
+++ app/models.py
@@ -13,17 +13,26 @@ class Author(db.Model):
     __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), index=True, unique=True)
-    books = db.relationship('Book', backref='author')
+    books = db.relationship('Title', backref='author')
 
     def __repr__(self):
         return '<Author %r>' % (self.name)
 
+class Title(db.Model):
+    __tablename__='titles'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(128), index=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    copies = db.relationship('Book', backref='title')
+
+    def __repr__(self):
+        return '<Book-Title %r, id:%r>' % (self.title, self.id)
+
 class Book(db.Model):
     __tablename__ = 'books'
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(128), index=True)
     borrower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    title_id = db.Column(db.Integer, db.ForeignKey('titles.id'))
 
     def __repr__(self):
-        return '<Book %r, id:%r>' % (self.title, self.id)
+        return '<Book-Copy %r, id:%r>' % (self.title, self.id)
