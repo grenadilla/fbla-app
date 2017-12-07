cfb9c16a9f2030d33151bbb84d287db45227cba1
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2017-12-07 21:01:41 +0000

    Updated form rendering and added cancel button

--- app/static/links.js
+++ app/static/links.js
@@ -2,4 +2,7 @@ $(document).ready(function($) {
     $(".clickable-link").click(function() {
         window.location.href = $(this).data("href");
     });
+    $(".cancel-button").click(function() {
+        history.go(-1);
+    });
 });
