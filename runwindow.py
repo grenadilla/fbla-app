30afbe146274311cdf6ac3c00b1b4fa1acf36830
grenadilla <33062689+grenadilla@users.noreply.github.com>
2018-01-14 13:30:01 -0600

    Added comments to runwindow.py

--- runwindow.py
+++ runwindow.py
@@ -5,6 +5,7 @@ import webview
 import run
 
 def url_ok(url, port):
+    # Check if server is on
     try:
         from http.client import HTTPConnection
     except ImportError:
@@ -20,15 +21,18 @@ def url_ok(url, port):
         return False
 
 def window():
+    # Create the app window
     webview.create_window("Flask Test", "http://127.0.0.1:5000", min_size=(600,800))
 
 if __name__ == '__main__':
+    # Start server in a seperate thread
     t = Thread(target=run.run_app, args=("127.0.0.1",))
     t.daemon = True
     t.start()
 
+    # Keep checking until the server is on
     while not url_ok("127.0.0.1", 5000):
         sleep(0.1)
-
+    
     Timer(1,window).start()
     webview.load_url("http://127.0.0.1:5000")
